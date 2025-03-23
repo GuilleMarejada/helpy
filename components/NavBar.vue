d4df3239c0d6de7871a67d9087ea9a6a64f90fad
Guillermo C.G <guillermo.gil.cor@gmail.com>
2025-03-23 14:09:29 +0000

    add NavBar component and update app layout; include Pinia module in configuration

---
+++ components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="bg-blue-500 p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <a href="#" class="text-white text-lg font-bold">Helpy</a>
+            <ul class="flex space-x-4">
+                <li><a href="#" class="text-white hover:text-gray-200">Home</a></li>
+                <li><a href="#" class="text-white hover:text-gray-200">About</a></li>
+                <li><a href="#" class="text-white hover:text-gray-200">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
