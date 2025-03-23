68430f49c36373f6b949a6372e109ea201089168
Guillermo C.G <guillermo.gil.cor@gmail.com>
2025-03-23 15:15:03 +0000

    update NavBar component with dynamic links and add theme colors in CSS

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,12 +1,44 @@
 <template>
-    <nav class="bg-blue-500 p-4">
-        <div class="container mx-auto flex justify-between items-center">
-            <a href="#" class="text-white text-lg font-bold">Helpy</a>
+    <nav class="text-azul">
+        <div class="bg-azul text-white mx-auto flex justify-between items-center px-8 py-4">
+            <a href="#" class="text-lg font-bold">Helpy</a>
             <ul class="flex space-x-4">
-                <li><a href="#" class="text-white hover:text-gray-200">Home</a></li>
-                <li><a href="#" class="text-white hover:text-gray-200">About</a></li>
-                <li><a href="#" class="text-white hover:text-gray-200">Contact</a></li>
+                <li v-for="link in mainLinks" :key="link.text">
+                    <a :href="link.href" class="">{{ link.text }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="container mx-auto flex justify-center mt-5">
+            <ul class="flex space-x-4">
+                <li v-for="service in services" :key="service.text">
+                    <a :href="service.href" class="border border-gris rounded-md p-2 hover:bg-gris">
+                        {{ service.text }}
+                    </a>
+                </li>
             </ul>
         </div>
     </nav>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                mainLinks: [
+                    { text: "Home", href: "#" },
+                    { text: "Cuenta", href: "#" },
+                    { text: "Trabaja con nosotros", href: "#" },
+                ],
+                services: [
+                    { text: "Fontaneria", href: "#" },
+                    { text: "Electricidad", href: "#" },
+                    { text: "Carpinteria", href: "#" },
+                    { text: "Jardineria", href: "#" },
+                    { text: "Pintura", href: "#" },
+                    { text: "Cerrajeria", href: "#" },
+                    { text: "Albañileria", href: "#" },
+                ],
+            };
+        },
+    };
+</script>
